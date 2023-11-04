6599d210dc9ff06432d7e42ac9093a87b977b4cb
Chandan Singh <74015019+chandan7953@users.noreply.github.com>
2023-11-04 20:19:53 +0530

    first commit

---
+++ src/store/reducer.cart.js
@@ -0,0 +1,20 @@
+const initialState = {
+    cart : []
+}
+
+function cartReducer(state = initialState,action){
+    if(action.type === 'cart'){
+        return {cart:[...state.cart,action.payload]}
+    }
+    else if(action.type === 'remove'){
+        return {cart: [...state.cart.filter((arrItem) => arrItem.id !== action.removeItem.id)]}
+    }
+
+    else if(action.type === 'reset'){
+        return {cart:[]};
+    }
+    
+    return state;
+}
+
+export default cartReducer;
